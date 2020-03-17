61e6eec7c6934e4b7df8830cfe9c85aebfd04a11
김도균 <dkkop225@gamil.com>
2020-03-17 23:58:36 +0900

    findInclude

---
+++ findInclude.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const arr = ['node.js','올인원']
+
+const ret = arr.find(key => key === '올인원')
+const res = arr.includes('node.js')
+console.log(res)
+
+
+// if(arr.includes('node.js')){
+
+// }
+// for(const item of arr) {
+//     if(arr.includes(item))
+// }
+//조건만을 검사하고 싶을땐 include
+//검사 목적 조건일땐 include , 조건만 필요할 때 
+
