1baedc710d8d8ec7357250dcb4a8e2acfb74e14c
김도균 <dkkop225@gamil.com>
2020-03-22 23:39:36 +0900

    templateString

---
+++ templateString.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const details ='자세한 내용'
+let str = 'node.js'
+str += `올인원 패키지 ${details}`
+const int = 1
+str +=`${str}의 값은${int}`
+
+//template String은 ₩으로 열고 닫아서 안에 ${변수명} 넣어주는 방식으로 사용
+
+
+//styled-components.com
