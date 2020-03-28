dd24d94555fcd4aea263df30c319f4427d3cb2ad
김도균 <dkkop225@gamil.com>
2020-03-28 22:08:56 +0900

    class2

---
+++ class2.js
@@ -0,0 +1,26 @@
+class Robot {
+    constructor(name) {
+        this.name = name
+    }
+
+    speak() {
+        console.log(`${this.name}`)
+    }
+}
+
+class Ai extends Robot{
+    constructor(name) {
+        super(name)
+    }
+
+    walk(){
+        console.log(`walk:${this.name}`)
+    }
+}
+const r = new Robot('hi')
+r.speak()
+
+
+const a= new Ai('hi')
+a.speak()
+a.walk()
