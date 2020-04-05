9883cab5162d300baa7d868277d4a2851e1fdbe1
김도균 <dkkop225@gamil.com>
2020-04-05 22:10:53 +0900

    adopter Pattern

---
+++ adopterPattern.js
@@ -0,0 +1,5 @@
+// adopter 패턴
+// 클래스의 인터페이스를 사용자가 기대하는 다른 인터페이스로 변환하는 패턴
+//호환성이 없는 인터페이스때문에 함께 동작할수 없는 클래스들이 함께 작동하도록 해주는 패턴
+
+//https://github.com/fastlecture/javascript-design-patterns-for-humans
