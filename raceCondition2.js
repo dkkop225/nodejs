8c00f9450f66d5f83405d04200fd5ea6bb99b2d4
김도균 <dkkop225@gamil.com>
2020-04-07 23:49:22 +0900

    raceCondition2

---
+++ raceCondition2.js
@@ -0,0 +1,28 @@
+let total = 0;
+
+async function getTotal(){
+    return total
+}
+async function setTotal(value){
+    return value
+}
+
+async function increment(value,inc) {
+    return value + inc
+}
+
+async function add() {
+    let current, newValue;
+    current = await getTotal()
+    newValue = await increment(current,20)
+    await setTotal(newValue)
+}
+
+async function main() {
+    const transaction1, transaction2
+    transaction1 = add()
+    transaction2 = add()
+    await transaction1 
+    await transaction2
+    console.log(await getTotal())
+}
